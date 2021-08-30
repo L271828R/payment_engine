63b1e49e320c8cbd8ee99a00da6351676b4aecb8
symbiont-luis-rueda <luis.rueda@symbiont.io>
2021-08-30 14:16:20 -0400

    code refactoring

--- payment_engine/core/engine.py
+++ payment_engine/core/engine.py
@@ -1,5 +1,8 @@
 import csv
 
+class TotalsHelper():
+    pass
+
 class PaymentEngine():
     def __init__(self, write_on_update=True):
         self.write_on_update = write_on_update
@@ -43,81 +46,95 @@ class PaymentEngine():
                 transactions_by_clients[client_id].append({'tx': tx,'type':trans_type, 'amount': amount})
             return transactions_by_clients
 
+    def _deposits_logic(self, client, tx_id, transaction, totals):
+        totals.tx_id_amount_lookup[tx_id] = transaction["amount"]
+        totals.total += float(transaction["amount"])
+        totals.available += float(transaction["amount"])
+        totals.clients[client] = {"client": client, "total" : totals.total, \
+            "available": totals.available, "held": totals.held, "locked": totals.locked}
+        assert(totals.total == totals.available + totals.held)
+
+    def _withdrawals_logic(self, client, tx_id, transaction, totals):
+        totals.tx_id_amount_lookup[tx_id] = transaction["amount"]
+        amount = float(transaction["amount"])
+        if amount <= totals.available: 
+            totals.total -= float(transaction["amount"])
+            totals.available -= float(transaction["amount"])
+            totals.clients[client] = {"client": client, "total" : totals.total, \
+                "available": totals.available, "held": totals.held, "locked": totals.locked}
+
+    def _disputes_logic(self, client, tx_id, transaction, disputed_resolution, totals):
+        if tx_id in totals.tx_id_amount_lookup and tx_id not in disputed_resolution:
+            amount = totals.tx_id_amount_lookup[tx_id]
+            totals.available -= amount
+            totals.held += amount
+            disputed_resolution[tx_id] = None
+            totals.total = totals.available + totals.held
+            totals.clients[client] = {"client": client, "total" : totals.total, \
+                "available": totals.available, "held": totals.held, "locked": totals.locked}
+            assert(totals.total == totals.available + totals.held)
+
+    def _resolves_logic(self, client, tx_id, transaction, disputed_resolution, totals):
+        if tx_id in totals.tx_id_amount_lookup and \
+            tx_id in disputed_resolution and \
+            disputed_resolution[tx_id]==None:
+            amount = totals.tx_id_amount_lookup[tx_id]
+            disputed_resolution[tx_id] = "resolve"
+            totals.held -= amount
+            totals.available += amount
+            totals.total = totals.available + totals.held
+            totals.clients[client] = {"client": client, "total" : totals.total, \
+                "available": totals.available, "held": totals.held, "locked": totals.locked}
+            assert(totals.total == totals.available + totals.held)
+
+    def _chargeback_logic(self, client, tx_id, transaction, disputed_resolution, totals):
+        if tx_id in totals.tx_id_amount_lookup and \
+            tx_id in disputed_resolution and \
+            disputed_resolution[tx_id]==None:
+            amount = totals.tx_id_amount_lookup[tx_id]
+            disputed_resolution[tx_id] = "chargeback"
+            #available += amount
+            totals.held -= amount
+            totals.locked = "true"
+            totals.total = totals.available + totals.held
+            totals.clients[client] = {"client": client, "total" : totals.total, \
+                "available": totals.available, "held": totals.held, "locked": totals.locked}
+            assert(totals.total == totals.available + totals.held)
+
     def _process_transactions(self, transactions_by_clients):
         clients = {}
         disputed_resolution = {}
+        totals = TotalsHelper()
+        totals.clients ={}
         for client in transactions_by_clients:
-            tx_id_amount_lookup = {}
-            total = 0.0
-            held = 0.0
-            available = 0.0
-            locked = 'false'
-            clients[client] = {"locked": "false"}
+            totals.tx_id_amount_lookup = {}
+            totals.total = 0.0
+            totals.held = 0.0
+            totals.available = 0.0
+            totals.locked = 'false'
+            totals.clients[client] = {"locked": "false"}
             for transaction in transactions_by_clients[client]:
                 trans_type = transaction['type']
                 tx_id = transaction['tx']
-                if clients[client]["locked"] == "true":
+                if totals.clients[client]["locked"] == "true":
                     is_locked = True
                 else:
                     is_locked = False
                 if trans_type == "deposit" and not is_locked:
-                    if tx_id not in disputed_resolution:
-                        tx_id_amount_lookup[tx_id] = transaction["amount"]
-                        total += float(transaction["amount"])
-                        available += float(transaction["amount"])
-                        clients[client] = {"client": client, "total" : total, \
-                            "available": available, "held": held, "locked": locked}
-                        assert(total == available + held)
-                        if self.write_on_update:
-                            self.update_clients_accounts_file(clients)
-
+                    self._deposits_logic(client, tx_id, transaction, totals)
+                    if self.write_on_update:
+                        self.update_clients_accounts_file(totals.clients)
                 elif trans_type == "withdrawal" and not is_locked:
-                    tx_id_amount_lookup[tx_id] = transaction["amount"]
-                    amount = float(transaction["amount"])
-                    if amount <= available: 
-                        total -= float(transaction["amount"])
-                        available -= float(transaction["amount"])
-                        clients[client] = {"client": client, "total" : total, \
-                            "available": available, "held": held, "locked": locked}
-                        assert(total == available + held)
-                        if self.write_on_update:
-                            self.update_clients_accounts_file(clients)
+                    self._withdrawals_logic(client, tx_id, transaction, totals)
+                    if self.write_on_update:
+                        self.update_clients_accounts_file(totals.clients)
                 elif trans_type == "dispute" and not is_locked:
-                    if tx_id in tx_id_amount_lookup and tx_id not in disputed_resolution:
-                        amount = tx_id_amount_lookup[tx_id]
-                        available -= amount
-                        held += amount
-                        disputed_resolution[tx_id] = None
-                        total = available + held
-                        clients[client] = {"client": client, "total" : total, \
-                            "available": available, "held": held, "locked": locked}
-                        assert(total == available + held)
-                        if self.write_on_update:
-                            self.update_clients_accounts_file(clients)
+                    self._disputes_logic(client, tx_id, transaction, disputed_resolution, totals)
+                    if self.write_on_update:
+                        self.update_clients_accounts_file(totals.clients)
                 elif trans_type == "resolve" and not is_locked:
-                    if tx_id in tx_id_amount_lookup and \
-                        tx_id in disputed_resolution and \
-                        disputed_resolution[tx_id]==None:
-                        amount = tx_id_amount_lookup[tx_id]
-                        disputed_resolution[tx_id] = "resolve"
-                        held -= amount
-                        available += amount
-                        total = available + held
-                        clients[client] = {"client": client, "total" : total, \
-                           "available": available, "held": held, "locked": locked}
-                        assert(total == available + held)
+                    self._resolves_logic(client, tx_id, transaction, disputed_resolution, totals)
                 elif trans_type == "chargeback" and not is_locked:
-                    if tx_id in tx_id_amount_lookup and \
-                        tx_id in disputed_resolution and \
-                        disputed_resolution[tx_id]==None:
-                        amount = tx_id_amount_lookup[tx_id]
-                        disputed_resolution[tx_id] = "chargeback"
-                        #available += amount
-                        held -= amount
-                        locked = "true"
-                        total = available + held
-                        clients[client] = {"client": client, "total" : total, \
-                           "available": available, "held": held, "locked": locked}
-                        assert(total == available + held)
-                assert(total == available + held)
-        return clients
+                    self._chargeback_logic(client, tx_id, transaction, disputed_resolution, totals)
+                assert(totals.total == totals.available + totals.held)
+        return totals.clients
