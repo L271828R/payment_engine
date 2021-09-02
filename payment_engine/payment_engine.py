e95e19bb696d159a81c14f2b673885109c3c581b
symbiont-luis-rueda <luis.rueda@symbiont.io>
2021-09-01 23:48:43 -0400

    adding requirement.txt

--- payment_engine/payment_engine.py
+++ payment_engine/payment_engine.py
@@ -1,28 +1,13 @@
 import sys
 from core.engine import PaymentEngine 
 
-#TODO make look nice
-#TODO test cases
-#TODO make test names better
 #TODO what if chargeback does not point back to dispute
 #TODO raise any unsupported transaction types
-#TODO test different file spacing in the csv
-#TODO check for file type of the csv file
-#TODO output to CLI as instructed
-
 
 def main():
     path_to_file = sys.argv[1]
     payment_engine = PaymentEngine(print_on_update=True)
     payment_engine.extract_transactions_by_client(path_to_file)
-    # data = {'1': [
-    #         {'tx': '1', 'type': 'deposit', 'amount': 1.0}, 
-    #         {'tx': '3', 'type': 'deposit', 'amount': 2.0}, 
-    #         {'tx': '4', 'type': 'withdrawal', 'amount': 1.5},
-    #         {'tx': '1', 'type': 'dispute', }
-    #         ], 
-    #     }
-    # PaymentEngine.transactions_by_clients = data
     payment_engine.process_transactions()
     accounts = payment_engine.clients_accounts
     payment_engine.update_clients_accounts_file(accounts)
